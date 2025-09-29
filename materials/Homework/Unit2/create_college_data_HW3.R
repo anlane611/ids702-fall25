464140e5885d475bc3e61e84b65c12cae8513ac6
Andrea Lane <andrea.lane@duke.edu>
2025-09-29 01:52:10 -0400

    HW3P1

---
+++ materials/Homework/Unit2/create_college_data_HW3.R
@@ -0,0 +1,5 @@
+## Clean college dataset for HW3
+## IDS 702 Fall 2025
+
+college_clean <- read.csv("/Users/andrealane/Documents/Teaching/IDS702-fall25/materials/homework/Unit2/college-cleaned.csv")
+
